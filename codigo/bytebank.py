2eb835d31c4d394dda85e19df7da369ef4640562
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2024-03-20 17:42:33 -0300

    Outro cenário para testar

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class Funcionario:
     def __init__(self, nome, data_nascimento, salario):
         self._nome = nome
@@ -19,6 +20,12 @@ class Funcionario:
         ano_nascimento = int(data_nascimento_quebrada[-1])
         ano_atual = date.today().year
         return ano_atual - ano_nascimento
+
+    def sobrenome(self):
+        nome_completo = self._nome.strip()
+        nome_quebrado = nome_completo.split(' ')
+        print(nome_quebrado)
+        return nome_quebrado[-1]
 
     def calcular_bonus(self):
         valor = self.salario * 0.1
--- testes/test_bytebank.py
+++ testes/test_bytebank.py
@@ -1,7 +1,8 @@
-from codigo import bytebank
 from codigo.bytebank import Funcionario
 
+
 class TestClass:
+
     def test_quando_idade_recebe_16_04_199_deve_retornar_25(self):
 
         # Given - Contexto
@@ -14,4 +15,14 @@ class TestClass:
         resultado = funcionario_teste.idade()
 
         # Then - desfecho
-        assert resultado == esperado 
+        assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_jorge_kayode_deve_retornar_kayode(self):
+        entrada = 'Jorge Kayodê'
+        esperado = 'Kayodê'
+
+        funcionario_teste = Funcionario(entrada, '16/04/1999', 1111)
+
+        resultado = funcionario_teste.sobrenome()
+
+        assert resultado == esperado
