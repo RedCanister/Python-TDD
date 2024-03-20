fa9f9dcd1aaa90422d06300c08f859011b9ff0d0
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2024-03-20 16:29:22 -0300

    Criando o primeiro teste

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -8,21 +8,23 @@ class Funcionario:
 
     @property
     def nome(self):
-        return self._nome
+        return self.nome
 
     @property
     def salario(self):
-        return self._salario
+        return self.salario
 
     def idade(self):
+        data_nascimento_quebrada = self._data_nascimento.split('/')
+        ano_nascimento = int(data_nascimento_quebrada[-1])
         ano_atual = date.today().year
-        return ano_atual - int(self._data_nascimento)
+        return ano_atual - ano_nascimento
 
     def calcular_bonus(self):
-        valor = self._salario * 0.1
+        valor = self.salario * 0.1
         if valor > 1000:
             valor = 0
         return valor
 
     def __str__(self):
-        return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario})'
+        return f'Funcionario({self.nome}, {self._data_nascimento}, {self.salario})'
